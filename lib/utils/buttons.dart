8ca2b5a827f365f10717d4b3023879c6d945a17f
jithinbhaskar11 <jithinbhaskar11@gmail.com>
2024-02-09 12:19:49 +0530

    completed

--- lib/utils/buttons.dart
+++ lib/utils/buttons.dart
@@ -5,22 +5,26 @@ class MyButton extends StatelessWidget {
   final color;
   final textColor;
   final String? buttonText;
+  final tapResult;
 
-  MyButton({this.color, this.textColor, this.buttonText});
+  MyButton({this.color, this.textColor, this.buttonText,this.tapResult});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Container(
-        decoration: BoxDecoration(
-          color: color,
-          shape: BoxShape.circle,
-          boxShadow: [BoxShadow(offset: Offset(2, 2),color: Colors.black.withOpacity(0.4),blurRadius: 2)]
-        ),
-              // color: color,
-              child: Center(child: Text(buttonText!,style: TextStyle(color: textColor,fontWeight: FontWeight.bold,fontSize: 14),)),
-            ),
+      child: GestureDetector(
+        onTap: tapResult,
+        child: Container(
+          decoration: BoxDecoration(
+            color: color,
+            shape: BoxShape.circle,
+            boxShadow: [BoxShadow(offset: Offset(1,1),color: Colors.black.withOpacity(0.4),blurRadius: 3)]
+          ),
+                // color: color,
+                child: Center(child: Text(buttonText!,style: TextStyle(color: textColor,fontWeight: FontWeight.bold,fontSize: 18),)),
+              ),
+      ),
     );
   }
 }
--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:calculator/utils/buttons.dart';
 import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,65 +13,101 @@ class _HomePageState extends State<HomePage> {
 
   var buttonText=[
     'C','DEL','%','/',
-    '9','8','7','x',
-    '6','5','4','-',
-    '3','2','1','+',
-    '0','.','ANS','=',
+    '7','8','9','x',
+    '4','5','6','-',
+    '1','2','3','+',
+    '0','00','.','=',
   ];
 
-  var problome='question';
-  var answer='answer';
+  var problem='';
+  var answer='';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     backgroundColor: Colors.deepPurple[50],
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Expanded(child: Container(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Container(
-                    padding: EdgeInsets.all(20),
-                    alignment: Alignment.centerLeft,
-                      child: Text(problome)),
-                  Container(
-                    padding: EdgeInsets.all(20),
-                      alignment: Alignment.centerRight,
-                      child: Text(answer))
-                ],
-              ),
-            )),
-            Expanded(
-                flex: 2,
-                child: Container(
-                  child:GridView.builder(
-                    itemCount: buttonText.length,
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4), itemBuilder: (context, index) {
-                    if (index==0){
-                     return MyButton(
-                        buttonText: buttonText[index],
-                        color: Colors.green,
-                        textColor: Colors.white,
-                      );
-                    }else if(index==1){
-                     return MyButton(
-                        buttonText: buttonText[index],
-                        color: Colors.red,
-                        textColor: Colors.white,
-                      );
-                    }else
-                     return  MyButton(
-                        buttonText: buttonText[index],
-                        color: isOperator(buttonText[index])?Colors.amber[600]: Colors.white,
-                        textColor: Colors.black,
-                      );
-                  },)
-                )),
-          ],
+      backgroundColor: Colors.white,
+     //backgroundColor: Colors.deepPurple[50],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Expanded(child: Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(20),
+                      alignment: Alignment.centerLeft,
+                        child: Text(problem,style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),)),
+                    Divider(),
+                    Container(
+                      padding: const EdgeInsets.all(20),
+                        alignment: Alignment.centerRight,
+                        child: Text(answer,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),))
+                  ],
+                ),
+              )),
+              Expanded(
+                  flex: 3,
+                  child: Container(
+                    child:GridView.builder(
+                      itemCount: buttonText.length,
+                      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4), itemBuilder: (context, index) {
+                        ///clear button
+                      if (index==0){
+                       return MyButton(
+                         tapResult: (){
+                           setState(() {
+                             problem='';
+                             answer='';
+                           });
+                         },
+                          buttonText: buttonText[index],
+                          color: Colors.green,
+                          textColor: Colors.white,
+                        );
+                       ///delete button
+                      }else if(index==1){
+                       return MyButton(
+                         tapResult: (){
+                           setState(() {
+                             problem=problem.substring(0,problem.length-1);
+                           });
+                         },
+                          buttonText: buttonText[index],
+                          color: Colors.red,
+                          textColor: Colors.white,
+                        );
+                       ///equal button
+                      }else if(index==buttonText.length-1){
+                        return MyButton(
+                          tapResult: (){
+                            setState(() {
+                              equalPressed();
+                            });
+                          },
+                          buttonText: buttonText[index],
+                          color: Colors.amber[600],
+                          textColor: Colors.black,
+                        );
+        
+                       //other buttons
+                      }else
+                       return  MyButton(
+                         tapResult: (){
+                           setState(() {
+                             problem+=buttonText[index];
+                           });
+                         },
+                          buttonText: buttonText[index],
+                          color: isOperator(buttonText[index])?Colors.amber[600]: Colors.white,
+                          textColor: Colors.black,
+                        );
+                    },)
+                  )),
+            ],
+          ),
         ),
       ),
     );
@@ -80,5 +117,18 @@ class _HomePageState extends State<HomePage> {
       return true;
     }else
       return false;
+  }
+
+  void equalPressed() {
+    String finalProblome=problem;
+    finalProblome=finalProblome.replaceAll('x', '*');
+    finalProblome = finalProblome.replaceAll('%', '/100*');
+
+    Parser p = Parser();
+    Expression exp = p.parse(finalProblome);
+    ContextModel cm = ContextModel();
+    double eval = exp.evaluate(EvaluationType.REAL, cm);
+
+    answer=eval.toString();
   }
 }
